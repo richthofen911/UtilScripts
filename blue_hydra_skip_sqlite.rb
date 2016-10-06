ec173e07ce12326173d6dbd6c620e089965b90fc
Yichao Li <yichaoli.richthofen@gmail.com>
2016-10-06 17:20:30 -0400

    Update and rename blue_hydra_skip_sqlite to blue_hydra_skip_sqlite.rb

---
+++ blue_hydra_skip_sqlite.rb
@@ -0,0 +1,37 @@
+# Skip the step of reading blue hydra data from its sqlite db. Instead, directly sending the required data to 
+# mysql db and influx db
+# Add this in Blue Hydra's parser.rb
+
+require 'mysql2'
+require 'influxdb'
+
+def connect_to_db_mysql(hostname, username, password, dbname)
+  mysql = Mysql2::Client.new(:host => hostname, :username => username, :password => password, :database => dbname)
+  return mysql
+end
+
+def connect_to_db_influx(dbname)
+  influx = InfluxDB::Client.new dbname
+  return influx
+end
+
+# insert into mysql
+=begin example of data format for mysql
+"insert into #{tablename_mysql} (Addr, UUID) VALUES ('ab:cd:ef', '15717843')"
+=end
+def insert_into_mysql(client, tablename, columns, values)
+  client.query("insert into #{tablename} #{columns} VALUES #{values}")
+end
+
+# insert into influx
+=begin example of data format for influx
+data = {
+  values: { value: Value.next },
+  tags:   { wave: 'sine' } # tags are optional
+}
+=end
+def insert_into_influx(client, measurement, data)
+  client.write_point(measurement, data)
+end
+
+
